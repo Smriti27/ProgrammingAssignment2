193544559449e61cca814b633b7d4cb25474b535
Smriti Kumari <smriti.kandhway@gmail.com>
2016-09-14 11:17:15 +0530

    Removing unwanted comments in R assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,6 @@
 ## Below are a pair of functions that are used to create a special object that 
 ## stores a matrix and caches its inverse.
 
-## Write a short comment describing this function
 ## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -28,8 +27,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## makeCacheMatrix above. If the inverse has already been calculated (and the 
 ## matrix has not changed), then it should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 		 inv <- x$getInverse()
@@ -42,3 +39,4 @@ cacheSolve <- function(x, ...) {
         x$setInverse(inv)
         inv
 }
+
